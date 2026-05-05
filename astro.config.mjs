f6e6e97e551728db63e2ecd86387429c5bb00ddb
Radu <you@example.com>
2026-05-05 21:09:02 +0300

    chore: setup github actions deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+  // Replace with your actual GitHub info
   site: 'https://alexaraducristian.github.io',
-  base: 'playground',
+  base: 'playground', 
 });
